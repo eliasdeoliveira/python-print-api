f5b6812016de034a4655bb3b78a842caa79c3914
Elias de Oliveira <eliasdeoliveirabr@gmail.com>
2024-09-16 23:39:13 -0300

    Primeiro commit

---
+++ app.py
@@ -0,0 +1,238 @@
+import traceback
+from flask import Flask, request, jsonify
+import win32print
+import win32ui
+import win32api
+import win32con
+import json
+import textwrap  # Para quebra de linha automática
+
+app = Flask(__name__)
+
+
+def format_json_to_table(data):
+    lines = []
+    try:
+        # Verifica se 'header' está presente no JSON
+        divider = '=' * 39
+        if 'header' in data:
+            header = data['header']
+            lines.append(header.get('companyName', 'Nome da Empresa'))
+            # Adiciona o divisor e a quebra de linha juntos
+            lines.append(divider)
+            lines.append('CUPOM NÃO FISCAL'.center(40))
+            if (header.get('numberOrder')):
+                # Adiciona o divisor e a quebra de linha juntos
+                lines.append(divider)
+                lines.append(f"Número do Pedido: {header.get('numberOrder')}".capitalize())
+        else:
+            # lines.append('Nome da Empresa')
+            lines.append('CUPOM NÃO FISCAL'.center(50))
+        lines.append(divider)  # Adiciona o divisor e a quebra de linha juntos
+
+        # Adiciona as colunas
+        # lines.append(f"{'Index':<5} {'Produto':<20} {'Cor':<10} {'Tamanho':<10} {'Qtd':<5} {'Preço':<10}")
+
+        # Comanda
+        if (data['command']):
+            lines.append(f"COMANDA: {data['command']}")
+            lines.append(divider)
+
+        # Verifica se 'items' está presente e é uma lista
+        if 'items' in data and isinstance(data['items'], list):
+            # Ajuste '40' para a largura da sua impressora
+            lines.append("PRODUTOS".center(50))
+            lines.append(divider)
+            for item in data['items']:
+                # lines.append(f"# {item.get('index', ''):<5} {item.get('productName', ''):<20} {item.get('productColor', ''):<10} {
+                #  item.get('productSize', ''):<10} {item.get('quantity', ''):<5} {item.get('totalPrice', ''):<10}")
+                lines.append(f"# {item.get('index', '')}) {
+                             item.get('productName', '')}")
+                if (item.get('productColor')):
+                    lines.append(f"Cor: {item.get('productColor', ''):<10} ")
+                if (item.get('productSize')):
+                    lines.append(
+                        f"Tamanho: {item.get('productSize', ''):<10} ")
+                if (item.get('unitPrice')):
+                    lines.append(f"Preço Unitário: {
+                                 item.get('unitPrice', ''):<10} ")
+                if (item.get('quantity')):
+                    lines.append(f"Quantidade: {
+                                 item.get('quantity', ''):<10} ")
+                if (item.get('totalPrice')):
+                    lines.append(
+                        f"Total: R$: {item.get('totalPrice', ''):<10} ")
+
+                # Itens adicionais
+                if 'additionalItems' in item and isinstance(item['additionalItems'], list):
+                    # Pula duas linhas
+                    lines.append("\n.")  # Linha em branco
+                    lines.append("ADICIONAIS:")
+                    for additional in item['additionalItems']:
+                        if (additional.get('productColor')):
+                            lines.append(
+                                f"Cor: {additional.get('productColor', ''):<10} ")
+                        if (additional.get('productSize')):
+                            lines.append(
+                                f"Tamanho: {additional.get('productSize', ''):<10} ")
+                        if (additional.get('unitPrice')):
+                            lines.append(f"Preço Unitário: {
+                                         additional.get('unitPrice', ''):<10} ")
+                        if (additional.get('quantity')):
+                            lines.append(f"Quantidade: {
+                                additional.get('quantity', ''):<10} ")
+                        if (additional.get('totalPrice')):
+                            lines.append(
+                                f"Total: R$: {additional.get('totalPrice', ''):<10} ")
+
+        # Verifica se 'paymentDetails' está presente
+        if 'paymentDetails' in data:
+            lines.append(divider)
+            lines.append("DETALHES DE PAGAMENTO".center(30))
+            lines.append(divider)
+            payment = data['paymentDetails']
+            if (payment.get('paymentForm')):
+                lines.append(f"Forma de Pagamento: {
+                    payment.get('paymentForm', 'Não especificado')}")
+                lines.append(divider)
+            if (payment.get('paymentMethod')):
+                lines.append(f"Método de Pagamento: {payment.get(
+                    'paymentMethod', 'Não especificado')}")
+                lines.append(divider)
+            if (payment.get('total')):
+                lines.append(f"Total: {payment.get(
+                    'total', 'Não especificado')}")
+                lines.append(divider)
+            if (payment.get('cashDepositAmount')):
+                lines.append(f"Depósito em Dinheiro: {payment.get(
+                    'cashDepositAmount', 'Não especificado')}")
+                lines.append(divider)
+            if (payment.get('cardNumber')):
+                lines.append(f"Número de Parcelas: {payment.get(
+                    'numberInstallments', 'Não especificado')}")
+                lines.append(divider)
+            if (payment.get('discount')):
+                lines.append(f"Desconto: {payment.get(
+                    'discount', 'Não especificado')}")
+                lines.append(divider)
+            if (payment.get('finalAmount')):
+                lines.append(f"Valor Final: {payment.get(
+                    'finalAmount', 'Não especificado')}")
+                lines.append(divider)
+            if (payment.get('change')):
+                lines.append(f"Troco: {payment.get(
+                    'change', 'Não especificado')}")
+
+        lines.append(divider)  # Linha de separação
+
+        # Finaliza a impressão
+        lines.append("\x1D\x56\x00")  # Eject paper
+
+    except Exception as e:
+        print(f"Error in format_json_to_table: {e}")
+        print(traceback.format_exc())
+        raise
+
+    return "\n".join(lines)
+
+
+def print_text(text, printer_name, font_size, margins):
+    try:
+        # Abre a impressora especificada
+        hprinter = win32print.OpenPrinter(printer_name)
+
+        # Cria o dispositivo de contexto (DC) para a impressora
+        hdc = win32ui.CreateDC()
+        hdc.CreatePrinterDC(printer_name)
+
+        # Configura a fonte
+        font = win32ui.CreateFont({
+            "name": "Arial",
+            "height": font_size,
+            "weight": win32con.FW_NORMAL
+        })
+        hdc.SelectObject(font)
+        hdc.SetTextColor(win32api.RGB(0, 0, 0))  # Preto
+        hdc.SetBkMode(win32con.TRANSPARENT)
+
+        # Obtém a resolução da impressora (DPI)
+        dpi_x = hdc.GetDeviceCaps(win32con.LOGPIXELSX)
+        dpi_y = hdc.GetDeviceCaps(win32con.LOGPIXELSY)
+
+        # Converte a largura de 50mm para pixels (50mm * DPI / 25.4mm por polegada)
+        max_width_mm = 50
+        max_width_px = int(max_width_mm * dpi_x / 25.4)
+
+        # Aplica as margens
+        margin_left = max(margins.get('left', 10), 10)
+        margin_top = max(margins.get('top', 10), 10)
+        text_x = margin_left
+        text_y = margin_top
+
+        # Quebra o texto em linhas de acordo com a largura máxima
+        text_lines = []
+        for line in text.splitlines():
+            # Ajusta o valor para o tamanho da fonte
+            text_lines.extend(textwrap.wrap(line, width=max_width_px // 10))
+
+        # Inicia o trabalho de impressão
+        hdc.StartDoc("Print Job")
+        hdc.StartPage()
+
+        # Imprime o texto com margens e quebra automática de linha
+        for line in text_lines:
+            hdc.TextOut(text_x, text_y, line)
+            text_y += font_size  # Ajuste o espaçamento entre linhas conforme necessário
+
+        # Finaliza a página e o trabalho de impressão
+        hdc.EndPage()
+        hdc.EndDoc()
+
+        # Fecha a impressora
+        win32print.ClosePrinter(hprinter)
+    except Exception as e:
+        print(f"Error in print_text: {e}")
+        print(traceback.format_exc())
+        raise
+
+
+def string_to_json(json_string):
+    try:
+        # Converte a string JSON para um objeto Python (dicionário)
+        data = json.loads(json_string)
+        return data
+    except json.JSONDecodeError as e:
+        # Captura erros de decodificação JSON
+        print(f"Error decoding JSON: {e}")
+        return None
+    except Exception as e:
+        # Captura qualquer outro erro
+        print(f"Unexpected error: {e}")
+        return None
+
+
+@app.route('/print', methods=['POST'])
+def printRouter():
+    data = request.json
+    if not data:
+        return jsonify({'error': 'No data provided'}), 400
+
+    try:
+        text = format_json_to_table(data['text'])
+        printer_name = data.get('printer_name', win32print.GetDefaultPrinter())
+        font_size = data.get('font_size', 12)
+        margins = data.get('margins', {})
+
+        print_text(text, printer_name, font_size, margins)
+        return jsonify({'status': 'success'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/status', methods=['GET'])
+def status():
+    return jsonify({'status': 'API is running'}), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ interface.py
@@ -0,0 +1,214 @@
+import sys
+import subprocess
+import requests
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QTextEdit, QVBoxLayout, QWidget, QLabel, QComboBox, QSystemTrayIcon, QMenu, QAction, QCheckBox
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt, QThread, pyqtSignal, QCoreApplication
+import win32print
+import winreg
+import os
+
+
+class ApiProcess(QThread):
+    """Thread para gerenciar o processo da API"""
+    def __init__(self):
+        super().__init__()
+        self.process = None
+
+    def run(self):
+        # Executa o script da API
+        self.process = subprocess.Popen(
+            ['python', 'app.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = self.process.communicate()
+        if stdout:
+            print(stdout.decode())
+        if stderr:
+            print(stderr.decode())
+
+    def stop(self):
+        if self.process:
+            self.process.terminate()
+            self.process.wait()
+            self.process = None
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Printer Control")
+        self.setGeometry(100, 100, 500, 500)
+
+        self.status_label = QLabel("API Status: Unknown")
+        self.log_text = QTextEdit()
+        self.log_text.setReadOnly(True)
+
+        self.print_button = QPushButton("Print Text")
+        self.print_button.clicked.connect(self.print_text)
+
+        self.api_status_button = QPushButton("Check API Status")
+        self.api_status_button.clicked.connect(self.check_api_status)
+
+        self.start_api_button = QPushButton("Start API")
+        self.start_api_button.clicked.connect(self.start_api)
+
+        self.stop_api_button = QPushButton("Stop API")
+        self.stop_api_button.clicked.connect(self.stop_api)
+
+        self.exit_button = QPushButton("Exit Application")
+        self.exit_button.clicked.connect(self.close_application)
+
+        self.printer_combo = QComboBox()
+        self.font_size_combo = QComboBox()
+        self.margin_left_combo = QComboBox()
+        self.margin_top_combo = QComboBox()
+        self.margin_right_combo = QComboBox()
+        self.margin_bottom_combo = QComboBox()
+
+        # Adiciona tamanhos de fontes e margens
+        self.font_size_combo.addItems(['21', '22', '24', '26', '28', '30'])
+        self.margin_left_combo.addItems(['10', '20', '30', '40', '50'])
+        self.margin_top_combo.addItems(['10', '20', '30', '40', '50'])
+        self.margin_right_combo.addItems(['10', '20', '30', '40', '50'])
+        self.margin_bottom_combo.addItems(['10', '20', '30', '40', '50'])
+
+        self.load_printers()
+
+        self.start_with_windows_checkbox = QCheckBox("Start with Windows")
+        self.start_with_windows_checkbox.setChecked(self.check_startup_status())
+        self.start_with_windows_checkbox.stateChanged.connect(self.set_startup_status)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.status_label)
+        layout.addWidget(self.log_text)
+        layout.addWidget(QLabel("Select Printer:"))
+        layout.addWidget(self.printer_combo)
+        layout.addWidget(QLabel("Select Font Size:"))
+        layout.addWidget(self.font_size_combo)
+        layout.addWidget(QLabel("Left Margin:"))
+        layout.addWidget(self.margin_left_combo)
+        layout.addWidget(QLabel("Top Margin:"))
+        layout.addWidget(self.margin_top_combo)
+        layout.addWidget(QLabel("Right Margin:"))
+        layout.addWidget(self.margin_right_combo)
+        layout.addWidget(QLabel("Bottom Margin:"))
+        layout.addWidget(self.margin_bottom_combo)
+        layout.addWidget(self.print_button)
+        layout.addWidget(self.api_status_button)
+        layout.addWidget(self.start_api_button)
+        layout.addWidget(self.stop_api_button)
+        layout.addWidget(self.exit_button)
+        layout.addWidget(self.start_with_windows_checkbox)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+        self.api_process = ApiProcess()
+
+        # Configura o ícone da bandeja do sistema
+        self.tray_icon = QSystemTrayIcon(QIcon('icon.png'), self)
+        self.tray_icon.setToolTip('Printer Control Application')
+
+        # Cria o menu da bandeja do sistema
+        tray_menu = QMenu()
+        restore_action = QAction("Restore")
+        restore_action.triggered.connect(self.restore_window)
+        exit_action = QAction("Exit")
+        exit_action.triggered.connect(self.close_application)
+        tray_menu.addAction(restore_action)
+        tray_menu.addAction(exit_action)
+
+        self.tray_icon.setContextMenu(tray_menu)
+        self.tray_icon.show()
+
+        # Adiciona evento para mostrar a janela ao clicar no ícone da bandeja
+        self.tray_icon.activated.connect(self.tray_icon_activated)
+
+    def load_printers(self):
+        # Lista as impressoras disponíveis
+        printers = win32print.EnumPrinters(win32print.PRINTER_ENUM_LOCAL)
+        printer_names = [printer[2] for printer in printers]
+        self.printer_combo.addItems(printer_names)
+
+    def print_text(self):
+        text = '{ "width": "58mm", "padding": "1mm", "header": { "companyName": "Gestor Ninja", "title": "Exemplo de Cupom Fiscal" }, "items": [ { "index": 1, "productName": "Nome do Produto", "productColor": "Cor do Produto", "productSize": "Tamanho do Produto", "quantity": 1, "totalPrice": "R$ 00,00", "additionalItems": [ { "index": 1, "productName": "Nome do Adicional", "productColor": "Cor do Adicional", "productSize": "Tamanho do Adicional", "quantity": 1, "totalPrice": "R$ 00,00" } ] }, { "index": 1, "productName": "Nome do Produto", "productColor": "Cor do Produto", "productSize": "Tamanho do Produto", "quantity": 1, "totalPrice": "R$ 00,00", "additionalItems": [ { "index": 1, "productName": "Nome do Adicional", "productColor": "Cor do Adicional", "productSize": "Tamanho do Adicional", "quantity": 1, "totalPrice": "R$ 00,00" }, { "index": 1, "productName": "Nome do Adicional", "productColor": "Cor do Adicional", "productSize": "Tamanho do Adicional", "quantity": 1, "totalPrice": "R$ 00,00" } ] } ], "paymentDetails": { "paymentForm": "Forma de Pagamento", "paymentMethod": "Método de Pagamento", "total": "R$ 00,00", "cashDepositAmount": "R$ 00,00", "numberInstallments": 1, "discount": "R$ 00,00", "finalAmount": "R$ 00,00", "change": "R$ 00,00" } }'
+        printer_name = self.printer_combo.currentText()
+        font_size = int(self.font_size_combo.currentText())
+        margins = {
+            'left': max(int(self.margin_left_combo.currentText()), 10),
+            'top': max(int(self.margin_top_combo.currentText()), 10),
+            'right': max(int(self.margin_right_combo.currentText()), 10),
+            'bottom': max(int(self.margin_bottom_combo.currentText()), 10)
+        }
+        try:
+            response = requests.post('http://127.0.0.1:5000/print', json={
+                'text': text, 'printer_name': printer_name, 'font_size': font_size, 'margins': margins})
+            self.log_text.append(f"Print Request Status: {response.status_code}")
+            self.log_text.append(f"Response: {response.json()}")
+        except requests.RequestException as e:
+            self.log_text.append(f"Error: {e}")
+
+    def check_api_status(self):
+        try:
+            response = requests.get('http://127.0.0.1:5000/status')
+            if response.status_code == 200:
+                self.status_label.setText("API Status: Running")
+            else:
+                self.status_label.setText("API Status: Not Running")
+        except requests.RequestException as e:
+            self.status_label.setText(f"API Status: Error - {e}")
+
+    def start_api(self):
+        self.api_process.start()
+        self.status_label.setText("API Status: Starting")
+
+    def stop_api(self):
+        self.api_process.stop()
+        self.status_label.setText("API Status: Stopped")
+
+    def restore_window(self):
+        self.showNormal()
+
+    def close_application(self):
+        self.api_process.stop()
+        QCoreApplication.instance().quit()
+
+    def set_startup_status(self, state):
+        if state == Qt.Checked:
+            self.add_to_startup()
+        else:
+            self.remove_from_startup()
+
+    def add_to_startup(self):
+        try:
+            key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Software\Microsoft\Windows\CurrentVersion\Run', 0, winreg.KEY_SET_VALUE)
+            winreg.SetValueEx(key, 'PrinterControlApp', 0, winreg.REG_SZ, sys.executable + ' ' + os.path.abspath(__file__))
+            winreg.CloseKey(key)
+        except Exception as e:
+            self.log_text.append(f"Error setting startup: {e}")
+
+    def remove_from_startup(self):
+        try:
+            key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Software\Microsoft\Windows\CurrentVersion\Run', 0, winreg.KEY_SET_VALUE)
+            winreg.DeleteValue(key, 'PrinterControlApp')
+            winreg.CloseKey(key)
+        except Exception as e:
+            self.log_text.append(f"Error removing startup: {e}")
+
+    def check_startup_status(self):
+        try:
+            key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Software\Microsoft\Windows\CurrentVersion\Run', 0, winreg.KEY_READ)
+            value, _ = winreg.QueryValueEx(key, 'PrinterControlApp')
+            winreg.CloseKey(key)
+            return True
+        except FileNotFoundError:
+            return False
+
+    def tray_icon_activated(self, reason):
+        if reason == QSystemTrayIcon.Trigger:
+            self.restore_window()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
